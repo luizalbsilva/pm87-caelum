62641fc0783090f4d5102214ba71d418fd4f3e3a
Luiz Alberoni da Silva <luizalbsilva@gmail.com>
2012-11-26 21:53:16 -0200

    primeiro commit do projeto

---
+++ src/br/com/caelum/pm87/cap2_11/Ex2_11.java
@@ -0,0 +1,13 @@
+package br.com.caelum.pm87.cap2_11;
+
+public class Ex2_11 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Exemplo de teste de uso do git.");
+
+	}
+
+}
